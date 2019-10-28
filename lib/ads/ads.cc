c696d4c4c29364351a62625a7fc0e97b13ef52eb
Andrew Casper <andrew@argosindustries.com>
2019-10-28 16:39:36 -0500

    Compiling cleanup.

--- lib/ads/ads.cc
+++ lib/ads/ads.cc
@@ -1,16 +1,115 @@
 #include <stdint.h>
+#include <cstring>
+
+#include "nrf_drv_spi.h"
+#include "nrf_log.h"
+#include "nrf_log_ctrl.h"
 
 #include "lib/ads/ads.h"
 
-ADS::ADS(const ads_pins_t& ads_pins){
-  pins_ = ads_pins;
-}
+namespace argos {
+  namespace ads {
 
-bool ADS::init(){
+    volatile bool spi_xfer_done;
 
-  return true;
-}
+    void spi_event_handler(nrf_drv_spi_evt_t const * p_event,
+                           void *                    p_context)
+    {
+      spi_xfer_done = true;
+    }
+    
+    ADS::ADS(const nrf_drv_spi_t& spi, const ads_pins_t& ads_pins):
+      spi_(spi),
+      pins_(ads_pins)
+    {
+      spi_xfer_done = false;
+    }
 
-int32_t ADS::get_data(){
-  return 0;
+    bool ADS::Init(){
+  
+      return true;
+    }
+
+    void ADS::WriteRegister(uint8_t address, uint8_t value) {
+      memset(tx_buf_, 0, kBufferLength);
+      memset(rx_buf_, 0, kBufferLength);
+      spi_xfer_done = false;
+      tx_buf_[0] = 0x40 + address;
+      tx_buf_[1] = 1;
+      tx_buf_[2] = value;
+
+      APP_ERROR_CHECK(nrf_drv_spi_transfer(&spi_, tx_buf_, 3, rx_buf_, 3));
+
+      while (!spi_xfer_done) {
+        __WFE();
+      }
+
+      return;
+    }
+
+    uint8_t ADS::ReadRegister(uint8_t address) {
+      memset(tx_buf_, 0, kBufferLength);
+      memset(rx_buf_, 0, kBufferLength);
+      spi_xfer_done = false;
+      tx_buf_[0] = 0x20 + address;
+      tx_buf_[1] = 1;
+
+      APP_ERROR_CHECK(nrf_drv_spi_transfer(&spi_, tx_buf_, 3, rx_buf_, 3));
+
+      while (!spi_xfer_done) {
+        __WFE();
+      }
+
+      return rx_buf_[2];
+    }
+
+    void ADS::DebugWriteRegister(uint8_t address, uint8_t value) {
+      uint8_t debug_value;
+      debug_value = ReadRegister(address);
+      NRF_LOG_INFO("Value %d: %x", address, debug_value);
+      WriteRegister(address, value);
+      debug_value = ReadRegister(address);
+      NRF_LOG_INFO("Value %d: %x", address, debug_value);
+      NRF_LOG_FLUSH();
+    }
+
+    void ADS::SendCommand(ADS1x9xCommand_t command){
+      memset(tx_buf_, 0, kBufferLength);
+      memset(rx_buf_, 0, kBufferLength);
+      spi_xfer_done = false;
+      tx_buf_[0] = command;
+      APP_ERROR_CHECK(nrf_drv_spi_transfer(&spi_, tx_buf_, 1, rx_buf_, 1));
+
+      while (!spi_xfer_done) {
+        __WFE();
+      }
+
+      return;
+    }
+
+    int32_t ADS::GetData() {
+      int32_t raw_value;
+      memset(tx_buf_, 0, kBufferLength);
+      memset(rx_buf_, 0, kBufferLength);
+      spi_xfer_done = false;
+      APP_ERROR_CHECK(nrf_drv_spi_transfer(&spi_, tx_buf_, 6, rx_buf_, 6));
+
+      while (!spi_xfer_done) {
+        __WFE();
+      }
+
+      raw_value = 0;
+      if (rx_buf_[3] & 0x80) {
+        raw_value = 0xff;
+      }
+      raw_value <<= 8;
+      raw_value += rx_buf_[3];
+      raw_value <<= 8;
+      raw_value += rx_buf_[4];
+      raw_value <<= 8;
+      raw_value += rx_buf_[5];
+      return raw_value;
+    }
+    
+  }
 }
--- lib/ads/ads.h
+++ lib/ads/ads.h
@@ -3,28 +3,64 @@
 
 #include <stdint.h>
 
-typedef struct {
-  int miso;
-  int mosi;
-  int csn;
-  int rst;
-  int drdy;
-  int start;
-  int clksel;
-} ads_pins_t;
+namespace argos {
+  namespace ads {
 
-class ADS {
+    const int kBufferLength = 6;
+    
+    typedef struct {
+      int miso;
+      int mosi;
+      int csn;
+      int rst;
+      int drdy;
+      int start;
+      int clksel;
+    } ads_pins_t;
 
- public:
-  ADS(const ads_pins_t& ads_pins);
+    typedef enum {
+      // System Commands
+      ADS_CMND_WAKEUP    = 0x02,   // Wake-up from standby mode
+      ADS_CMND_STANDBY   = 0x04,   // Enter standby mode
+      ADS_CMND_RESET_CMD = 0x06,   // Reset the device registers
+      ADS_CMND_START     = 0x08,   // Start/restart (synchronize) conversions
+      ADS_CMND_STOP      = 0x0A,   // Stop conversion
+      ADS_CMND_OFFSETCAL = 0x1A,   // Channel offset calibration - needs to be sent every time there is a change to the PGA gain
+      // Data Read Commands
+      ADS_CMND_RDATAC    = 0x10,   // Enable Read Data Continuous mode.
+      // - This mode is the default mode at power-up.
+      ADS_CMND_SDATAC    = 0x11,   // Stop Read Data Continuously mode
+      ADS_CMND_RDATA     = 0x12,   // Read data by command; supports multiple read back.
+      // Register Read/Write Commands
+      ADS_CMND_RREG      = 0x20,   // Read n nnnn registers starting at address r rrrr
+      //  - first byte 001r rrrr (2xh)(2) - second byte 000n nnnn(2)
+      ADS_CMND_WREG      = 0x40    // Write n nnnn registers starting at address r rrrr
+      //  - first byte 010r rrrr (2xh)(2) - second byte 000n nnnn(2)
+    } ADS1x9xCommand_t;
 
-  bool init();
 
-  int32_t get_data();
+    class ADS {
 
- private:
-  ads_pins_t pins_;
-  
-};
+    public:
+      ADS(const nrf_drv_spi_t& spi, const ads_pins_t& ads_pins);
+
+      bool Init();
+
+      int32_t GetData();
+
+      void WriteRegister(uint8_t address, uint8_t value);
+      uint8_t ReadRegister(uint8_t address);
+      void DebugWriteRegister(uint8_t address, uint8_t value);
+      void SendCommand(ADS1x9xCommand_t command);
+      
+    private:  
+      const nrf_drv_spi_t spi_;
+      ads_pins_t pins_;
+      uint8_t tx_buf_[kBufferLength];
+      uint8_t rx_buf_[kBufferLength];
+      
+    };
+  }
+}
 
 #endif
