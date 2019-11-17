3bceb2df689e263563d101e875ffebd62fa8f10f
Andrew Casper <andrew@argosindustries.com>
2019-11-17 10:30:06 -0600

    Decimates more.

--- projects/proto_fw_argos/src/main.c
+++ projects/proto_fw_argos/src/main.c
@@ -271,9 +271,9 @@ void timer_event_handler(nrf_timer_event_t event_type, void* p_context)
               NRF_LOG_DEBUG("Size: %d", sizeof(ble_arrhythmia));
               ble_ecg_arrhythmia_send(&m_nus, &ble_arrhythmia, &ble_arrhythmia_size, m_conn_handle);
             }
-            // Some functions are only called every 120ms (e.g. every 6th time through this function).
+            // TODO(andy) Double check this number.
             call_count++;
-            call_count %= 6;
+            call_count %= 15;
             break;
         default:
             //Do nothing.
